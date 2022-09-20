3699d1a60ace60ac8bf9ded51718d84de85c02fa
Sixto Bazan <>
2022-09-20 17:28:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+#Sixto bazan 4 d
+
+multiplicar = int (input("ingresa el numero a crear tabla : "))
+
+
+multiplos = int (input("ingresa el numero de multiplos : "))
+
+
+for i in range (1,multiplos+1) : 
+  m = i * multiplicar
+
+  print(multiplicar,"*",i,"=", m)  
